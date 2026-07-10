7029e32d0e92811cd57e373d4c2530c8d2c816c1
TimanhNguyen <100458231+Timqtzy@users.noreply.github.com>
2026-07-11 00:18:26 +0800

    fix: layout for sidebar and main

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -3,10 +3,10 @@ import { Sidebar } from "./sidebar"
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="app-shell">
+    <div className="app-shell flex">
       <Sidebar />
-      <main className="app-main">
-        <Navbar />
+      <main className="app-main w-full">
+          {/*<Navbar />*/}
         {children}
       </main>
     </div>
